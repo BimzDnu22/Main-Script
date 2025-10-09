28fd914d68fbf00159744d022611dda0582d84a4
BimzDnu22 <sbimz6621@gmail.com>
2025-10-09 21:41:12 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/1a78ea4b68b742f69256a9553ee54270"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/1afd2a8f50c34d84be0a88c12bc28597"))()
