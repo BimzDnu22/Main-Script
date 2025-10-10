46768ed12e8272120c2ef0fe3c6f9a52c6d25f4a
BimzDnu22 <sbimz6621@gmail.com>
2025-10-10 21:07:44 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/b840b157e5474a0ca889405408238e4c"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/9abf9269d68640ba80becead8ff7de7c"))()
